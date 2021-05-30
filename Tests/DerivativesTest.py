abcce01c1757faf770e229ee95bd6161cec5e8e9
Ramandeep Singh <bunny112001@gmail.com>
2021-05-30 20:25:07 +0530

    Cleaned The Repo a Little

--- Tests/DerivativesTest.py
+++
@@ -1,4 +0,0 @@
-import unittest,sys,datetime
-sys.path.insert(0,'..')
-from NSE.companies import Companies
-print(Companies().HistoricalData('RBLBANK',Series=[Companies().EQ,Companies().RL],TimePeriod='1y'))
