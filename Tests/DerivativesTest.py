7232faaab2bce7a5a3bac6c87f7b66eb1a892ab3
Ramandeep Singh <bunny112001@gmail.com>
2021-05-30 19:58:16 +0530

    Fixed Json Open Issues

--- Tests/DerivativesTest.py
+++ Tests/DerivativesTest.py
@@ -1,4 +1,4 @@
 import unittest,sys,datetime
 sys.path.insert(0,'..')
 from NSE.companies import Companies
-print(Companies().HistoricalData('RBLBANK',Series=[r'%22EQ%22',r'%22RL%22'],TimePeriod='1y'))
+print(Companies().HistoricalData('RBLBANK',Series=[Companies().EQ,Companies().RL],TimePeriod='1y'))
